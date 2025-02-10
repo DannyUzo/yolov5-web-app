c5ae847f842b02fb06dd94da15a4998988e3162e
DannyUzo <uzodinmadaniel42@gmail.com>
2025-02-10 18:43:59 +0100

    Added requirements.txt

--- yolo-backend/app.py
+++ yolo-backend/app.py
@@ -34,5 +34,5 @@ def detect_objects():
 
     return jsonify({"detections": detections})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True)
